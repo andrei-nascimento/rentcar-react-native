a592c52a71a99c7a61c905dbf37d505baf373980
Andrei Nascimento <andrei.lima.nas@outlook.com>
2022-11-25 17:56:46 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Intro from './src/screens/Intro';
+import {
+  useFonts,
+  Nunito_900Black_Italic
+} from '@expo-google-fonts/nunito';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Nunito_900Black_Italic
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Intro />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Intro.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function Intro() {
+    return (     
+        <LinearGradient 
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+                colors={['#19779B','#17B3A6']}>
+
+            <View style={styles.logomarca}>
+                <Image style={styles.logo}
+                    source={require('../../assets/imgs/volante-branco.png')} />
+                <Text style={styles.title}>Rentcar</Text>
+            </View>
+
+            <StatusBar style="auto" />
+        </LinearGradient>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    logomarca: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 32,
+        fontFamily: 'Nunito_900Black_Italic',
+        color: '#fff',
+    },
+    logo: {
+        width: 32,
+        height: 32,
+        marginRight: 6
+    }
+});
